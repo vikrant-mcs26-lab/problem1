11ad27af233e097c6bbf23ae7788cbf1661904cf
vikrant <vikrant-mcs26@cs.du.ac.in>
2026-08-05 21:19:05 +0530

    Fixing bugs

--- cpp/src/graph_generator.cpp
+++ cpp/src/graph_generator.cpp
@@ -1,28 +1,29 @@
 #include <graph.hpp>
 #include <iostream>
 #include <filesystem>
+#include <cstdlib>
 
 int main(int argc, char **argv){
-    if (argc < 2){
-        std::cout << "Usage: ./graph_generator <directory>" << std::endl;
+    if (argc < 3){
+        std::cout << "Usage: ./graph_generator <seed> <directory>" << std::endl;
         return 1;
     }
 
-    std::string directory = argv[1];
-
-    if (directory.back() == '/'){
-        directory.pop_back();
-    }
-
+    std::filesystem::path directory = argv[2];
+    int seed = atoi(argv[1]);
+    
     if (!std::filesystem::is_directory(directory)){
         std::cout << "Directory does not exist." << std::endl;
         return 1;
     }
 
+    directory = directory / std::filesystem::path("graph_");
+
     for(int i = 10; i <= 45; i += 5){
         Graph *g = new Graph(10);
-        g -> generate_graph(1, i);
-        g -> save_graph(std::string(argv[1]) + "/graph_" + std::to_string(i) + ".v");
+        std::string path = directory.string() + std::to_string(i) + ".graph";
+        g -> generate_graph(seed, i);
+        g -> save_graph(path);
         delete g;
     }
     return 0;
